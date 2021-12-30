a224ea10ee69d313deebb8a7ba0c078bd1edbd2a
rusenmelih <m.rusenmelih@gmail.com>
2021-12-30 16:31:11 +0300

    Entity Framework Initialize

---
+++ backend/DataAccess/FISContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace DataAccess
+{
+    public class FISContext : DbContext
+    {
+        public FISContext()
+        {
+
+        }
+        public FISContext(DbContextOptions<FISContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Citizen> Citizens { get; set; }
+
+
+
+
+    }
+}
--- backend/VTYS.Entities/Citizen.cs
+++ backend/VTYS.Entities/Citizen.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace VTYS.Entities
+namespace Entities
 {
     public class Citizen
     {
