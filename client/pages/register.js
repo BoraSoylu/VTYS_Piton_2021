2785c1db7119bb2d041e292435e69a6d20524f9e
huseyin gulcu <huseyingulcuu@gmail.com>
2022-01-03 23:03:56 +0300

    feat: Add api.

--- client/pages/register.js
+++ client/pages/register.js
@@ -3,10 +3,26 @@
 import Header from "../components/Header/Header";
 
 import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
 
 const Register = () => {
   const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
   console.log(user, isAuthenticated);
+
+  useEffect(() => {
+    const signIn = async () => {
+      const response = await fetch("https://localhost:3000/api/auth/citizens/login", {
+        method: "POST",
+        body: JSON.stringify({
+          email: "hs@gmail.com",
+          password: "123456",
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+    };
+    signIn();
+  }, []);
 
   return (
     <>
